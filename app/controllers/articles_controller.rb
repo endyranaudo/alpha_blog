ceeab9c049ae513c0cfd2814b9c7c2cabb21aa1b
Endy Ranaudo <endy@wearesnook.com>
2020-07-31 17:46:28 +0100

    Added create method. Next step: front-end validations/error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
 
-  def show
-    # byebug
-    @article = Article.find(params[:id])
+  def index
+    @articles = Article.all
   end
 
-end 
+  def new
+  
+  end
+ 
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    # redirect_to article_path(@article) 
+    redirect_to @article #shorthand redirect, same us above
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+end
