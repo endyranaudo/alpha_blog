8f78d8edf813a23596214fe9ccce8a112559f49e
Endy Ranaudo <endy@wearesnook.com>
2020-08-03 15:33:09 +0100

    CRU done, need to do Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,18 +5,36 @@ class ArticlesController < ApplicationController
   end
 
   def new
-  
+    @article = Article.new
   end
  
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    # redirect_to article_path(@article) 
-    redirect_to @article #shorthand redirect, same us above
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      # redirect_to article_path(@article) 
+      redirect_to @article #shorthand redirect, same us above
+    else
+      render 'new'
+    end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   
 end
