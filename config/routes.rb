49e5a4c36d801e755852ee18f9c35a46b7479380
Endy Ranaudo <endy@wearesnook.com>
2020-08-04 08:51:59 +0100

    Before refactoring it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # SHORT HAND IF I NEED ALL THE RESTFUL ACTIONS
+  resources :articles
 end
