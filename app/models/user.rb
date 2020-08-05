7f7bc1e9259258642488d3dc146a9ad473848e65
Endy Ranaudo <endy@wearesnook.com>
2020-08-05 11:22:17 +0100

    before refactoring error partials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  #BEFORE_SAVE
+  before_save { self.email = email.downcase} 
   #VALIDATIONS
   validates :username, presence: true,  uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 35 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }
   #ASSOCIATIONS
   has_many :articles
+  #BCRYPT
+  has_secure_password
 end
