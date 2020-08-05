8b3a94eb37b4ff01d6e1b73daa75cb0b447887f7
Endy Ranaudo <endy@wearesnook.com>
2020-08-05 16:47:21 +0100

    Added Sign up functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # SHORT HAND IF I NEED ALL THE RESTFUL ACTIONS
   resources :articles
+
 end
